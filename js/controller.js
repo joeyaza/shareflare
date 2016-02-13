abc2750f5c87d85c6d390aa4f79cfc62c60bd0c7
joeyaza <JAzadehdel@gmail.com>
2016-02-13 14:17:05 +0000

    history api requested

--- js/controller.js
+++ js/controller.js
@@ -5,41 +5,62 @@ angular
 MainController.$inject = ['$http'];
 function MainController($http){
   var self = this;
-  this.all = [];
+  this.stock = [];
+  this.history = [];
   
-  this.ticker = "";
-  this.price = "";
-  this.name = "";
-  this.description = "";
-  this.marketHigh = "";
-  this.marketLow = "";
-  this.marketValue = "";
-  this.classification = "";
+  // this.ticker = "";
+  // this.price = "";
+  // this.name = "";
+  // this.description = "";
+  // this.marketHigh = "";
+  // this.marketLow = "";
+  // this.marketValue = "";
+  // this.classification = "";
 
-  function getStocks(){
+  function getSearch(){
 
     var stock = {
-      "term": "citigroup",
+      "term": "apple",
       "select": "_all"
     }
 
     $http
       .put('https://dozlacmd51.execute-api.us-east-1.amazonaws.com/v1/search', stock)
       .then(function(response){
-        console.log(response);
-        self.all = response.data[0];
-        self.price = self.all.price;
-        self.name = self.all.long_name;
-        self.ticker = self.all.ticker;
-        self.description = self.all.description;
-        self.marketHigh = self.all.fifty_two_week_high;
-        self.marketLow = self.all.fifty_two_week_low;
-        self.classification = self.all.classification;
-        self.marketValue = self.all.market_value;
+        self.stock = response.data[0];
+        self.price = self.stock.price;
+        self.name = self.stock.long_name;
+        self.ticker = self.stock.ticker;
+        self.description = self.stock.description;
+        self.marketHigh = self.stock.fifty_two_week_high;
+        self.marketLow = self.stock.fifty_two_week_low;
+        self.classification = self.stock.classification;
+        self.marketValue = self.stock.market_value;
       });
     
   }
 
-  getStocks();
+  function getHistory() {
+
+    var history = {
+      "sic": "6c8227be-6855-11e4-98bf-294717b2347c",
+      "after": 0,
+      "select": ["price", "market_value_usd"]
+    }
+
+    $http
+    .put('https://dozlacmd51.execute-api.us-east-1.amazonaws.com/v1/historical', history)
+    .then(function(response){
+      console.log(response);
+        // self.history = response.data[0];
+        // self.price = self.stock.price;
+
+
+      })
+
+  }
+
+  getSearch();
+  getHistory()
 
 }
