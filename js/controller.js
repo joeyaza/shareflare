f0ffbe2961df3acdeffd44e95c10582ab189609a
joeyaza <JAzadehdel@gmail.com>
2016-02-15 17:06:08 +0000

    started grabbing from the instrument api

--- js/controller.js
+++ js/controller.js
@@ -40,20 +40,34 @@ function MainController($http){
     
   }
 
+  function getInstrument() {
+
+    var instrument = {
+    "sic": "6c8227be-6855-11e4-98bf-294717b2347c"
+    }
+
+    $http
+      .put('https://dozlacmd51.execute-api.us-east-1.amazonaws.com/v1/instruments', instrument)
+      .then(function(response){
+        console.log(response);
+        self.instrument = response.data[0];
+    })
+  }
+
   function getHistory() {
 
     var history = {
       "sic": "6c8227be-6855-11e4-98bf-294717b2347c",
       "after": 0,
-      "select": ["dividends"]
+      "select": ["dividends", "return_on_equity"]
     }
 
     $http
     .put('https://dozlacmd51.execute-api.us-east-1.amazonaws.com/v1/historical', history)
     .then(function(response){
-      console.log(response);
       self.history = response.data[0];
       self.dividends = self.history.dividends;
+      self.roe = self.history.return_on_equity
         // self.history = response.data[0];
         // self.price = self.stock.price;
 
@@ -63,6 +77,7 @@ function MainController($http){
   }
 
   getSearch();
-  getHistory()
+  getHistory();
+  getInstrument();
 
 }
